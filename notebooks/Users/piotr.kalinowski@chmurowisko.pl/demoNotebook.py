116fa3f2ee2416d8c5d58a7f499e899d0c3a43d7
kali11 <kali.piotrek@gmail.com>
2021-06-11 11:06:29 +0200

    first notebook commit

---
+++ notebooks/Users/piotr.kalinowski@chmurowisko.pl/demoNotebook.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+print(y)
+
+# COMMAND ----------
+
+# DBTITLE 1,Loading data
+print(x + 3)
+y = 4
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # title
+# MAGIC *sfsdf*
+
+# COMMAND ----------
+
